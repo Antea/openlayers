ba1831f5bf3257feeeacb6946fad397ed06fc8f1
Federico Russo <russo.federico@anteash.com>
2013-01-15 08:53:03 +0100

    Added Google v3 and OSM.

--- build/bbox_build.sh
+++ build/bbox_build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-python build.py -c closure bbox OpenLayers.js
+python build.py -c jsmin bbox OpenLayers.js
 python buildUncompressed.py bbox OpenLayers.pretty.js
 mv OpenLayers.* ../../openlayers-vaadin-wrapper/src/main/resources/org/vaadin/vol/public/OpenLayers-2.12/
